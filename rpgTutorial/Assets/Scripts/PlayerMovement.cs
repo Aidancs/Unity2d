0caafa91ad4d5b4a64afa977cc06eb4b830274e9
Aidancs <aidancs316@gmail.com>
2016-12-04 20:16:25 -0600

    1st Commit
    Checking to see if it is the whole folder or the subfolder

---
+++ rpgTutorial/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D rBody;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		rBody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector2 movementVector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (movementVector != Vector2.zero)
+		{
+			anim.SetBool ("IsWalking", true);
+			anim.SetFloat ("Input_X", movementVector.x);
+			anim.SetFloat ("Input_Y", movementVector.y);
+		} 
+
+		else
+		{
+			anim.SetBool ("IsWalking", false);	
+		}
+
+		rBody.MovePosition (rBody.position + movementVector * Time.deltaTime);
+			
+	}
+}
---
+++ rpgTutorial/Assets/Scripts/Warp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+	public Transform warp_target;
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		Debug.Log ("an object collided");
+
+		other.gameObject.transform.position = warp_target.position;
+		Camera.main.transform.position = warp_target.position;
+	}
+
+}
